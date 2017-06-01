3c38f8cc12110dcd3bcfb102051c984898390bb5
Mauricio Saglietto <msaglietto@gmail.com>
2017-06-01 22:27:05 +0200

    Update ClassWithStyles.js
    Use PropTypes from prop-types not react

--- generators/component/templates/ClassWithStyles.js
+++ generators/component/templates/ClassWithStyles.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 import s from './styles.scss';
@@ -13,7 +14,7 @@ const defaultProps = {
 
 /**
  * Component - Description
- * @extends Components
+ * @extends Component
  */
 export class Component extends React.Component {
 
